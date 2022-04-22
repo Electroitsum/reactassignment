04dbd038cde19eca8995fb67287006082721dfce
SUMIT SHARMA <61307650+Electroitsum@users.noreply.github.com>
2022-04-22 20:00:55 +0530

    Add files via upload

---
+++ problem2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './style/style.css';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <div className='main-cont-holder'>
+      <Main></Main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
